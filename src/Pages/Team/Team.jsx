31d36d8ac5bdaf0601f64940668771710b08aaf9
Hieu Nguyen <minhhieun4@gmail.com>
2020-10-17 17:51:11 +0700

    fix team

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -3,6 +3,7 @@ import "./team.sass";
 import teamLogo from "../../images/body/team/team_logo.svg";
 import vietnameseImage from "../../images/body/team/vietnamese_team.svg";
 import japaneseImage from "../../images/body/team/japanese_team.svg";
+import member_list from "./data.js";
 
 const Team = () => {
   return (
@@ -17,8 +18,27 @@ const Team = () => {
       </div>
       <div className="team">
         <div className="team_logo">
-          <img src={teamLogo} alt="Vietnam Flag" />
+          <img src={teamLogo} alt="Team logo" />
         </div>
+
+        <div className="team_member">
+          {member_list.map((item, index) => (
+            <div className="team_member_item">
+              <div className="member_list_title">{item.title}</div>
+              <div className="member_list_line"></div>
+              <div className="member_list_details">
+                {item.member.map((item_member, index_member) => (
+                  <div className="team_member_item_member">
+                    <img src={item_member.member_image} alt="Member" />
+                    <div className="member_name">{item_member.member_name}</div>
+                  </div>
+                  // <div className="member_name">{item_member.member_name}</div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+
         <div className="team_details team_vietnam">
           <div className="team_details_image_wrapper">
             <div className="team_details_image">
---
+++ src/Pages/Team/data.js
@@ -0,0 +1,61 @@
+const member_list = [
+    {
+      title: "Optimal Deployment",
+      member: [
+        {
+          member_name: "Do Phan Thuan",
+          member_image: require("../../images/body/team/member/Do_Phan_Thuan.png")
+        },
+        {
+          member_name: "Huynh Thi Thanh Binh",
+          member_image: require("../../images/body/team/member/Huynh_Thi_Thanh_Binh.png")
+        },
+        {
+          member_name: "Nguyen Phi Le",
+          member_image: require("../../images/body/team/member/Nguyen_Phi_Le.png")
+        }
+      ]
+    },
+    {
+      title: "Opportunistic communication Air quality forecasting",
+      member: [
+        {
+          member_name: "Nguyen Kien",
+          member_image: require("../../images/body/team/member/Nguyen_Kien.png")
+        },
+        {
+          member_name: "Nguyen Phi Le",
+          member_image: require("../../images/body/team/member/Nguyen_Phi_Le.png")
+        }
+      ]
+    },
+    {
+      title: "Data Calibration",
+      member: [
+        {
+          member_name: "Le Minh Thuy",
+          member_image: require("../../images/body/team/member/Le_Minh_Thuy.png")
+        },
+        {
+          member_name: "Nguyen Phi Le",
+          member_image: require("../../images/body/team/member/Nguyen_Phi_Le.png")
+        }
+      ]
+    },
+    {
+      title: "Localization",
+      member: [
+        {
+          member_name: "Nguyen Cam Ly",
+          member_image: require("../../images/body/team/member/Nguyen_Cam_Ly.png")
+        },
+        {
+          member_name: "Le Minh Thuy",
+          member_image: require("../../images/body/team/member/Le_Minh_Thuy.png")
+        }
+      ]
+    }
+  ];
+  
+  export default member_list;
+  
